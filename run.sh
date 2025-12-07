45841b41ffcb07839fbfb235b57400456466704b
Thomas Zakrajsek <tzakrajs@linux.com>
2025-12-06 19:47:19 -0800

    * Uses docker.io/tzakrajs/ansible-runner instead of pipenv bullshit

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-#!/usr/bin/env bash
-ANSIBLE_ROLES_PATH=./roles ansible-playbook ${@:1}
+#!/bin/bash
+docker run --rm -it \
+  -v $(pwd):/runner \
+  -v ~/.ssh:/home/runner/.ssh:ro \
+  -e HOME=/home/runner \
+  -e ANSIBLE_REMOTE_USER=$(whoami) \
+  docker.io/tzakrajs/ansible-runner:2.20.0 \
+  ${@:1}
